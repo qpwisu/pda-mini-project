b5dcd28f22b4b4ef83505fa57c6de1410de30111
sinjeong-in <shjin4667@gmail.com>
2024-11-06 16:06:53 +0900

    first

--- pda-news-frontend/vite.config.js
+++ pda-news-frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    alias: {
+      '~/components': '/src/components',
+      '~/lib': '/src/lib',
+      '~/routes': '/src/routes',
+      '~/routers': '/src/routers',
+      '~/pages': '/src/pages',
+    },
+  },
+});
+
