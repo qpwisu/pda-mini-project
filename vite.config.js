88e5ed51e4c9c875e60dcaec4d68cc82cfe41aa8
DrivenByCode <jaykey1004@gmail.com>
2024-11-11 12:59:29 +0900

    fix: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 혹은 / 사용
   build: {
-      outDir: 'dist', // 출력 디렉토리 확인
+    outDir: 'dist', // 출력 디렉토리 확인
   },
   resolve: {
     alias: {
